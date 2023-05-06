0a65f3df1c465e370035882675a05d16b047adaa
NotBB <notice76610@gmail.com>
2023-05-06 11:17:39 +0200

    Create script.js

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+var images = ["assets/image/montagne.jpg", "assets/image/ocean.jpg", "assets/image/volcan.webp"];
+var imageElement = document.getElementById("image");
+var titleElement = document.getElementById("title");
+var paragraphElement = document.getElementById("paragraph");
+var inputElement = document.getElementById("input");
+var textareaElement = document.getElementById("textarea");
+
+function changerImage(index) {
+    if (index >= 0 && index < images.length) {
+        var nouvelleImage = images[index];
+        imageElement.src = nouvelleImage;
+    }
+}
+
+inputElement.addEventListener("input", function() {
+    titleElement.innerText = inputElement.value;
+});
+
+textareaElement.addEventListener("input", function() {
+    paragraphElement.innerText = textareaElement.value;
+});
+
+//Changer la Police
+function changerPolice(police) {
+    titleElement.style.fontFamily = police;
+    paragraphElement.style.fontFamily = police;
+}
+
+//Changer la couleur
+function changerCouleur(couleur) {
+    titleElement.style.color = couleur;
+    paragraphElement.style.color = couleur;
+}
+
+//Changer le style
+function changerStyle(style) {
+    titleElement.style.fontStyle = style;
+    paragraphElement.style.fontStyle = style;
+}
